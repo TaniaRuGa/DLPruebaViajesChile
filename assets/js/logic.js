628edcd24696e7cebeb55cfe794824af82f94578
Tania Ruiz Garay <taniaprg@gmail.com>
2024-07-21 12:52:11 -0400

    cambio en ajustar la velocidad del carousel en JS con la clase #carouselExample

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,3 @@
+const carousel = document.querySelector('#carouselExample'); // Selecciona el elemento del carrusel con la clase "#carouselExample"//
 
-
-
-
+carousel.options = 2000; // Ajusta la velocidad del carrusel en 2 segundos //
