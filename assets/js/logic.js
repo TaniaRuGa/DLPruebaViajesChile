266986c760ddba3e4b0d01c7f4e62d0a32f2587b
Tania Ruiz Garay <taniaprg@gmail.com>
2024-07-21 13:15:49 -0400

    agrega sroll al CSS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,3 @@
 const carousel = document.querySelector('#carouselExample'); // Selecciona el elemento del carrusel con la clase "#carouselExample"//
 
-carousel.options = 2000; // Ajusta la velocidad del carrusel en 2 segundos //
+carousel.options = 1500; // Ajusta la velocidad del carrusel en 1.5 segundos //
