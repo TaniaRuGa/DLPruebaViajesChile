c7c391f929241295fc26b7b85aa0e49215d0304f
Tania Ruiz Garay <taniaprg@gmail.com>
2024-07-22 19:05:42 -0400

    agregar jQuery a cards

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,26 @@
 const carousel = document.querySelector('#carouselExample'); // Selecciona el elemento del carrusel con la clase "#carouselExample"//
 
 carousel.options = 1500; // Ajusta la velocidad del carrusel en 1.5 segundos //
+
+$(document).ready(function(){
+    $("parrafoCard").dblclick(function(){
+        $(this).hide("slow");
+    });
+});
+// Selector para elementos con clase ""
+$(document).ready(function() {
+    $(".textD").click(function() {
+        $(this).css({
+        "color": "#BA23DB",
+        "font-size": "2em",
+        "font-weight": 900
+        });
+    });
+    $(".textD").dblclick(function() {
+        $(this).css({
+        "color": "white",
+        "font-size": "1.2em",
+        "font-weight": 400
+        });
+    });
+});
